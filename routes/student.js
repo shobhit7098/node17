0ccd1a357ceb46e86bd85ed0cc403cf7a8710348
shobhit7098 <sharma.shobhit7060@gmail.com>
2024-09-17 16:27:24 +0530

    updated msg...

--- routes/student.js
+++ routes/student.js
@@ -8,4 +8,7 @@ router.post('/add/user',(req,res)=>{
 router.get('/users',(req, res)=>{
         StudentController.getStudents(req,res);
 })
+router.get('/users/:rollNo',(req, res)=>{
+        StudentController.getStudentsByRollNo(req,res);
+})
 module.exports = router
